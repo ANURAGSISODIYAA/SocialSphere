e61ac1f70e8c2eb7d13e9e694b33ca1d66e1670f
Anurag Sisodiya <122148320+ANURAGSISODIYAA@users.noreply.github.com>
2024-05-08 11:04:54 +0530

    API_BASE_URL

--- src/Config/api.js
+++ src/Config/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const API_BASE_URL = "https://cozy-determination-production.up.railway.app/"
+export const API_BASE_URL = "https://cozy-determination-production.up.railway.app"
 
 const jwtToken=localStorage.getItem("jwt");
 
