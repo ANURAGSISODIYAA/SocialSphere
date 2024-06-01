cfcf22c9df6d5e87c86b0aa414ecb600948d31cc
Anurag Sisodiya <sisodiyaanurag2121@gmail.com>
2024-06-01 14:57:53 +0530

    Update CreateReelsForm.jsx

--- src/Components/Reels/CreateReelsForm.jsx
+++ src/Components/Reels/CreateReelsForm.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const CreateReelsForm = () => {
   return (
     <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', height: '100%' }}>
-      <p style={{ textAlign: 'center', color: '#ffff', fontSize: '2rem', marginTop: '32px' }}>This feature is on production!</p>
+      <p style={{ textAlign: 'center', color: '#ffff', fontSize: '2rem', marginTop: '32px' }}>This feature is on production !!</p>
       <p style={{ textAlign: 'center', color: '#ffff', fontSize: '1.5rem', marginTop: '16px' }}>Features like Home page, Create post, photo & video upload, like, comment, profile, change name, messaging/chat functionality, login, and registration are already implemented.</p>
     </div>
   );
